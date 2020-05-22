80f419c9d0f35914c3fff9932a20bb8e82dc527d
fbobrow <fbob@me.com>
2020-05-22 19:16:05 -0300

    Adjust attitude estimator correct step (nonlinear version)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 // Drivers example programas
-//#include "programs/examples/drivers/main_hall.h"
-//#include "programs/examples/drivers/main_imu.h"
-//#include "programs/examples/drivers/main_motor.h"
+// #include "programs/examples/drivers/main_hall.h"
+// #include "programs/examples/drivers/main_imu.h"
+// #include "programs/examples/drivers/main_motor.h"
 
 // Modules example programs
-//#include "programs/examples/modules/main_estimator_wheel.h"
-//#include "programs/examples/modules/main_estimator_attitude.h"
+// #include "programs/examples/modules/main_estimator_wheel.h"
+// #include "programs/examples/modules/main_estimator_attitude.h"
 #include "programs/examples/modules/main_controller_attitude_wheel.h"
 // #include "programs/examples/modules/main_controller_attitude_wheel_log.h"
--- src/modules/estimator_attitude.cpp
+++ src/modules/estimator_attitude.cpp
@@ -94,14 +94,39 @@ void AttitudeEstimator::predict(float omega_x, float omega_y, float omega_z)
 // Correct step
 void AttitudeEstimator::correct(float ax, float ay, float az)
 {    
-    // Calculate quaternion measurement
-    float q0_m =   ax*q2 - ay*q1 - az*q0;
-    float q1_m = - ax*q3 - ay*q0 + az*q1;
-    float q2_m =   ax*q0 - ay*q3 + az*q2;
-    float q3_m = - ax*q1 - ay*q2 - az*q3;
-    // Correct quaternion time derivative
-    q0 += lds*dt*(q0_m-q0);
-    q1 += lds*dt*(q1_m-q1);
-    q2 += lds*dt*(q2_m-q2);
-    q3 += lds*dt*(q3_m-q3);
+    // // Calculate rotation quaternion measurement
+    // float qm0 =   ax*q2 - ay*q1 - az*q0;
+    // float qm1 = - ax*q3 - ay*q0 + az*q1;
+    // float qm2 =   ax*q0 - ay*q3 + az*q2;
+    // float qm3 = - ax*q1 - ay*q2 - az*q3;
+    // // Correct rotation quaternion
+    // q0 += lds*dt*(qm0-q0);
+    // q1 += lds*dt*(qm1-q1);
+    // q2 += lds*dt*(qm2-q2);
+    // q3 += lds*dt*(qm3-q3);
+
+    // Calculate rotation quaternion measurement
+    float qm0 =   ax*q2 - ay*q1 - az*q0;
+    float qm1 = - ax*q3 - ay*q0 + az*q1;
+    float qm2 =   ax*q0 - ay*q3 + az*q2;
+    float qm3 = - ax*q1 - ay*q2 - az*q3;
+    // Calculate rotation quaternion error
+    float qe0 = q0*qm0 + q1*qm1 + q2*qm2 + q3*qm3;
+    float qe1 = q0*qm1 - q1*qm0 - q2*qm3 + q3*qm2;
+    float qe2 = q0*qm2 + q1*qm3 - q2*qm0 - q3*qm1;
+    float qe3 = q0*qm3 - q1*qm2 + q2*qm1 - q3*qm0;
+    // Calculate rotation Gibbs-vector error
+    float se1 = qe1/qe0;
+    float se2 = qe2/qe0;
+    float se3 = qe3/qe0;
+    // Calculate rotation quaternion error time derivative
+    float qe0_dot = - q1*se1 - q2*se2 - q3*se3;
+    float qe1_dot =   q0*se1 - q3*se2 + q2*se3;
+    float qe2_dot =   q3*se1 + q0*se2 - q1*se3;
+    float qe3_dot = - q2*se1 + q1*se2 + q0*se3;
+    // Correct rotation quaternion
+    q0 += lds*dt*qe0_dot;
+    q1 += lds*dt*qe1_dot;
+    q2 += lds*dt*qe2_dot;
+    q3 += lds*dt*qe3_dot;
 }
